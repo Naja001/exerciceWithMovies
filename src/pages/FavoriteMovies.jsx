26078318dc2e3b094b2ed38db30a72d220001766
Natalija Ojdanic <natalija.ojdanic@gmail.com>
2024-06-06 14:51:37 +0200

    Finished movie project using fetching data from dummyapi using axios

--- src/pages/FavoriteMovies.jsx
+++ src/pages/FavoriteMovies.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 
 const Favorites = () => {
@@ -11,7 +10,12 @@ const Favorites = () => {
         setFavorites(storedFavorites);
       }
     }, []);
-    console.log(favorites)
+  
+    const removeFromFavorites = (id) => {
+      const updatedFavorites = favorites.filter(favorite => favorite.id !== id);
+      setFavorites(updatedFavorites);
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+    };
   
     return (
       <div className='container'>
@@ -25,6 +29,10 @@ const Favorites = () => {
                   <h5 className="card-title">{favorite.movie}</h5>
                   <p className="card-text">Rating: {favorite.rating}</p>
                   <a href={favorite.imdb_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">View on IMDb</a>
+                  
+                </div>
+                <div>
+                <button onClick={() => removeFromFavorites(favorite.id)} className="btn btn-light mt-2">Remove from Favorites</button>
                 </div>
               </div>
             </div>
@@ -32,5 +40,6 @@ const Favorites = () => {
         </div>
       </div>
     );
-  };
-export default Favorites
+};
+
+export default Favorites;
